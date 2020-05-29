012d7390bdebdbb3f43d19466f89cc04a7c838b0
letItCurl <rolandlopez.developer@gmail.com>
2020-05-29 11:27:56 +0200

    ng basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
+import { AuthorService } from './author.service';
+import { CourseService } from './courses.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses.component';
+import { CourseComponent } from './course/course.component';
+import { AuthorsComponent } from './authors/authors.component';
+import { AuthorComponent } from './author/author.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    CourseComponent,
+    AuthorsComponent,
+    AuthorComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
-  ],
-  providers: [],
+],
+  providers: [CourseService,
+              AuthorService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/courses.service.ts
@@ -0,0 +1,6 @@
+
+export class CourseService {
+    getCourses(){
+        return ["Courses 1","Courses 2","Courses 3"]
+    }
+}
