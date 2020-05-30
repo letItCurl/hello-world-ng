f11388e1a8b60726632e7d3e60d83cbd98d39149
letItCurl <rolandlopez.developer@gmail.com>
2020-05-30 13:33:34 +0200

    *ng for study

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,9 +11,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hello-world-ng';
-  tweet: any
+  tweet: any;
+  id = 4;
 
-  courses = [1,2,3]
+  courses: Array<any> = [];
+
+  viewMode = "map";
 
   // Data injection / singleton patern
   constructor(service: LikeService){
@@ -21,8 +24,45 @@ export class AppComponent {
     this.tweet = service.getTweet() 
   }
 
+  onAdd(){
+    this.id = this.id + 1 
+    this.courses.push({id: this.id, name: `courses${this.id}` })
+  }
+
+  onRemove(course){
+    let index = this.courses.indexOf(course)
+    this.courses.splice(index,1)
+  }
+
+  onChange(course){
+    let index = this.courses.indexOf(course)
+    this.courses[index].name = `${index} UPDATE`
+  }
+
+  onChangeFor(course){
+    course.name = "SIMPLE UPDATE"
+
+    let index = this.courses.indexOf(course)
+    console.log(this.courses[index].name)
+  }
+  
+  loadCourses(){
+    this.courses = [
+      {id: 1, name: 'courses1'},
+      {id: 2, name: 'courses2'},
+      {id: 3, name: 'courses3'},
+      {id: 4, name: 'courses4'}
+    ];
+  }
+
+  // return the data to watch
+  trackCourse(index, course){
+    return course ? course.id : undefined
+  }
+
   onFourthStar(event: FavArgs){
     console.log('The 4th start has been clicked');
     console.log(event)
   }
+  
 }
