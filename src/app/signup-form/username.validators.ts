f65ba3fa4bbd8988dd7f5744f48dddcb14ba2a3a
letItCurl <rolandlopez.developer@gmail.com>
2020-05-31 15:58:09 +0200

    async validators

--- src/app/signup-form/username.validators.ts
+++ src/app/signup-form/username.validators.ts
@@ -8,4 +8,18 @@ export class UsernameValidators {
         }
         return null
     }
+
+    // async validator
+    static shouldBeUnique(control: AbstractControl): Promise<ValidationErrors | null>{
+        
+        return new Promise((resolve, reject)=>{
+            setTimeout(()=>{
+                console.log('ok')
+                if((control.value as string) == 'roro')
+                    resolve({ shouldBeUnique: true })
+                else resolve(null)
+            },900)
+        })
+
+    }
 }
